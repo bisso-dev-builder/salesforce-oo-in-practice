1fcf0fccec27938d8cee46c96567e326c7a2f441
ercarval <eduardo.carvalho@topi.io>
2024-02-13 11:06:35 -0300

    add fixure factories

--- deploy-challenge-resources.sh
+++ deploy-challenge-resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sfdx force:source:deploy --sourcepath force-app/main/default/objects/Promotion__c force-app/main/default/objects/PromotionalProduct__c force-app/main/default/permissionsets/OOInPractice.permissionset-meta.xml force-app/main/default/classes/PromotionalProduct.cls force-app/main/default/classes/Promotion.cls force-app/main/default/classes/PromotionalProductRepository.cls  force-app/main/default/classes/PromotionService.cls --target-org $1
+sfdx force:source:deploy --sourcepath force-app/main/default/objects/Promotion__c force-app/main/default/objects/PromotionalProduct__c force-app/main/default/permissionsets/OOInPractice.permissionset-meta.xml force-app/main/default/classes/PromotionalProduct.cls force-app/main/default/classes/Promotion.cls force-app/main/default/classes/PromotionalProductRepository.cls  force-app/main/default/classes/PromotionService.cls force-app/main/default/classes/ProductFixtureFactory.cls  force-app/main/default/classes/PromotionFixtureFactory.cls force-app/main/default/classes/PromotionalProductFixtureFactory.cls  --target-org $1
 
 sfdx force:apex:execute --file ./scripts/apex/challenge-01/create-promotions.apex --target-org $1
 
